ea12ab00bb2e4ac6ecb62a27c691dba904ee8e08
Emadabdi <155989126+Emadabdi@users.noreply.github.com>
2026-06-24 12:12:21 +0200

    Added coin collection sound effect

--- CoinCollector/Assets/Scripts/Coin.cs
+++ CoinCollector/Assets/Scripts/Coin.cs
@@ -1,22 +1,28 @@
 using UnityEngine;
 
+using UnityEngine;
+
 public class Coin : MonoBehaviour
 {
     public float rotationSpeed = 100f;
 
     void Update()
     {
-        transform.Rotate(
-    rotationSpeed * Time.deltaTime,
-    rotationSpeed * Time.deltaTime,
-    0
-);
+        transform.Rotate(rotationSpeed * Time.deltaTime, 0, 0);
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            AudioSource audioSource =
+                FindFirstObjectByType<AudioSource>();
+
+            if (audioSource != null)
+            {
+                audioSource.Play();
+            }
+
             GameManager.Instance.AddScore(1);
             Destroy(gameObject);
         }
